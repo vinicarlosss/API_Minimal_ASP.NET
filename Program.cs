0738daf63592f37d6c99a14daa5b1e95cb87dd73
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2025-09-01 12:13:58 -0300

    feat: adding dbcontext

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using MinimalApi.DTOs;
+using MinimalApi.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
 
 app.MapGet("/", () => "Hello World!");
 
