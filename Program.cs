195e2932a0f88946619418649bf05d0ecdb05ca1
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2025-09-14 21:56:49 -0300

    feat: Implementing GET route to veiculos

--- Program.cs
+++ Program.cs
@@ -45,6 +45,7 @@ app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico admin
 #endregion
 
 #region Veiculos
+
 app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
 {
     var veiculo = new Veiculo
@@ -56,6 +57,13 @@ app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veic
     veiculoServico.Incluir(veiculo);
     return Results.Created($"/veiculo{veiculo.Id}", veiculo);
 });
+
+app.MapGet("/veiculos", ([FromQuery]int? pagina, IVeiculoServico veiculoServico) =>
+{
+    var veiculos = veiculoServico.Todos(pagina);
+    return Results.Ok(veiculos);
+});
+
 #endregion
 
 #region App
