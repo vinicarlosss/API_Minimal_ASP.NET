adadd6ea7b5383ed7b17af90a67779ecf8a6f318
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2025-09-14 22:41:14 -0300

    feat: implementing validation to put and post veiculo

--- Program.cs
+++ Program.cs
@@ -46,8 +46,39 @@ app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico admin
 
 #region Veiculos
 
+ErrosDeValidacao validaDTO(VeiculoDTO veiculoDTO)
+{
+
+    var validacao = new ErrosDeValidacao
+    {
+        Mensagens = new List<string>()
+    };
+    if (string.IsNullOrEmpty(veiculoDTO.Nome))
+    {
+        validacao.Mensagens.Add("O nome não pode ser vazio");
+    }
+
+    if (string.IsNullOrEmpty(veiculoDTO.Marca))
+    {
+        validacao.Mensagens.Add("A marca não pode ficar em branco");
+    }
+
+    if (veiculoDTO.Ano < 1950)
+    {
+        validacao.Mensagens.Add("Veículo muito antigo, aceito somente anos superiores a 1950");
+    }
+    return validacao;
+}
+
 app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
 {
+
+
+    var validacao = validaDTO(veiculoDTO);
+    if (validacao.Mensagens.Count > 0)
+    {
+        return Results.BadRequest(validacao);
+    }
     var veiculo = new Veiculo
     {
         Nome = veiculoDTO.Nome,
@@ -75,6 +106,11 @@ app.MapPut("/veiculos/{id}", ([FromRoute] int id, VeiculoDTO veiculoDTO, IVeicul
 {
     var veiculo = veiculoServico.BuscaPorId(id);
     if (veiculo == null) return Results.NotFound();
+    var validacao = validaDTO(veiculoDTO);
+    if (validacao.Mensagens.Count > 0)
+    {
+        return Results.BadRequest(validacao);
+    }
     veiculo.Nome = veiculoDTO.Nome;
     veiculo.Marca = veiculoDTO.Marca;
     veiculo.Ano = veiculoDTO.Ano;
